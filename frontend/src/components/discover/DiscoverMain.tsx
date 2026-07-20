27c3b5b20f286cc899cb3101bde64e5ecfa8d30d
mina-houshmand <houshmand_mina@yahoo.com>
2026-07-20 16:32:11 +0200

    FEAT: wire Curated for You section into the Discover page

---
+++ frontend/src/components/discover/DiscoverMain.tsx
@@ -0,0 +1,10 @@
+import { CuratedForYouSection } from "./CuratedForYouSection";
+
+// HappeningNowSection will be added here once feature/discover-happeningNow is merged.
+export function DiscoverMain() {
+  return (
+    <div className="discover-main">
+      <CuratedForYouSection />
+    </div>
+  );
+}
