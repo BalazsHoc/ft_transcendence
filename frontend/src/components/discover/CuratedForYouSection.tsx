dfb3f59e3f03f943171e40426ff41fd3317f3a70
mina-houshmand <houshmand_mina@yahoo.com>
2026-07-20 16:32:11 +0200

    FIX: use real photo URLs instead of placeholder in Curated for You cards

--- frontend/src/components/discover/CuratedForYouSection.tsx
+++ frontend/src/components/discover/CuratedForYouSection.tsx
@@ -1,7 +1,9 @@
 import { useTranslation } from "react-i18next";
 import { CuratedEventCard } from "./CuratedEventCard";
 import { FeaturedEventCard } from "./FeaturedEventCard";
-import { DEFAULT_EVENT_IMAGE_SRC } from "../../utils/media";
+
+const FEATURED_IMAGE =
+  "https://lh3.googleusercontent.com/aida-public/AB6AXuDmj0MpYVy6qJULbayBJZHM2_kmzuFIqhYwGEvDwQoyZKtpyzVslCdg_qRVURfpdCrMxZMflWQh-y8qvcanTpdUn4rGV9DOdV0O6cT3OKsdMcyQWgHSYiPMhM0r8VqIpq1_hgzcAFUrzus3ic09kEXUXDdSxkm_gMAvYk0ePhfaPXL4pz3O0AU-5kuBv7q7I6xM_-tV3VdNBQ2SZVmfc5uR1tnKPLVQ7zCsbKDAPfTjeoXZy6qsK2CpAMO75M-s11PxwMTbvCxU-fc";
 
 export function CuratedForYouSection() {
   const { t } = useTranslation();
@@ -9,14 +11,15 @@ export function CuratedForYouSection() {
   const curatedEvents = [
     {
       id: "morning-flow",
-      image: DEFAULT_EVENT_IMAGE_SRC,
+      image: "https://www.gstatic.com/labs-code/stitch/stitch-placeholder-300x300.svg",
       title: t("discover.curatedEventOneTitle"),
       categoryLabel: t("discover.yoga"),
       timeLabel: t("discover.curatedEventOneTime"),
     },
     {
       id: "strength-conditioning",
-      image: DEFAULT_EVENT_IMAGE_SRC,
+      image:
+        "https://lh3.googleusercontent.com/aida-public/AB6AXuAWiXzlxBg5UVhX1jwi8Oo-229AaR5i-vGHDCcXT6oToxQXvuthslKxJUiSBnJBqg39nsKLlTHHTO5bqBE0cu6G4AjM9rVCxhmO-90qKQWGhJuWe1BZxjT1dODPDwMKK72Yrh4AmQ4JkUFtzwZV7ELpQ8QgBFhFKU1EkA1uDL5UhKBk0_aIQ-nKdKpdWRpF5PqlkUMod-kQWnjC-WN2o9BqOlUrcf-q8B2fZEaSvbioHp4-iGfSsI9guy6g9VIUyot1EJKfBPhafiQ",
       title: t("discover.curatedEventTwoTitle"),
       categoryLabel: t("discover.strength"),
       timeLabel: t("discover.curatedEventTwoTime"),
@@ -29,7 +32,7 @@ export function CuratedForYouSection() {
 
       <div className="curated-for-you__grid">
         <FeaturedEventCard
-          image={DEFAULT_EVENT_IMAGE_SRC}
+          image={FEATURED_IMAGE}
           title={t("discover.featuredEventTitle")}
           description={t("discover.featuredEventDescription")}
           levelLabel={t("discover.intermediate")}
