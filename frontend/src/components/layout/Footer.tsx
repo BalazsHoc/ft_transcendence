8e67c43d2110f811584f6bab525ec4d9b738dd9e
oshcheholev <oshcheho@student.42vienna.com>
2026-08-04 10:32:22 +0200

    UI: small footer adjustments + app name in index.html

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -1,3 +1,29 @@
+import Link from "react-router-dom";
+
 export function Footer() {
-  return <footer className="footer">Active Vienna API frontend</footer>;
+  return <footer className="footer">
+    <div className="footer-content">ft_transcendence - 42_Vienna Project</div>
+    <div className="contributors">
+      <span> (c) </span> 
+      <a href="mailto:mhoushma@student.42.fr" target="_blank" rel="noopener noreferrer">
+        mhoushma
+      </a>
+      <span>, </span>   
+      <a href="mailto:bhocsak@student.42.fr" target="_blank" rel="noopener noreferrer">
+        bhocsak
+      </a>
+      <span>, </span>
+      <a href="mailto:cjuarez@student.42.fr" target="_blank" rel="noopener noreferrer">
+        cjuarez
+      </a>
+      <span>, </span>
+      <a href="mailto:oshcheho@student.42.fr" target="_blank" rel="noopener noreferrer">
+        oshcheho
+      </a>
+      <span>, </span>
+      <a href="mailto:pghajard@student.42.fr" target="_blank" rel="noopener noreferrer">
+        pghajard
+      </a>
+    </div>
+    </footer>;
 }
